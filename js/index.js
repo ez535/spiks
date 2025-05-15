27e1a91d8f872f3e09678e147a75119adde6a6c6
Zhenya Eremkin <john2196mail@yandex.ru>
2025-05-15 14:42:23 +0300

    Update 2025-05-15T11:42:20.542Z

---
+++ js/index.js
@@ -0,0 +1,233 @@
+"use strict";
+
+function _typeof(o) { "@babel/helpers - typeof"; return _typeof = "function" == typeof Symbol && "symbol" == typeof Symbol.iterator ? function (o) { return typeof o; } : function (o) { return o && "function" == typeof Symbol && o.constructor === Symbol && o !== Symbol.prototype ? "symbol" : typeof o; }, _typeof(o); }
+function _toConsumableArray(arr) { return _arrayWithoutHoles(arr) || _iterableToArray(arr) || _unsupportedIterableToArray(arr) || _nonIterableSpread(); }
+function _nonIterableSpread() { throw new TypeError("Invalid attempt to spread non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); }
+function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(o); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
+function _iterableToArray(iter) { if (typeof Symbol !== "undefined" && iter[Symbol.iterator] != null || iter["@@iterator"] != null) return Array.from(iter); }
+function _arrayWithoutHoles(arr) { if (Array.isArray(arr)) return _arrayLikeToArray(arr); }
+function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) arr2[i] = arr[i]; return arr2; }
+function ownKeys(e, r) { var t = Object.keys(e); if (Object.getOwnPropertySymbols) { var o = Object.getOwnPropertySymbols(e); r && (o = o.filter(function (r) { return Object.getOwnPropertyDescriptor(e, r).enumerable; })), t.push.apply(t, o); } return t; }
+function _objectSpread(e) { for (var r = 1; r < arguments.length; r++) { var t = null != arguments[r] ? arguments[r] : {}; r % 2 ? ownKeys(Object(t), !0).forEach(function (r) { _defineProperty(e, r, t[r]); }) : Object.getOwnPropertyDescriptors ? Object.defineProperties(e, Object.getOwnPropertyDescriptors(t)) : ownKeys(Object(t)).forEach(function (r) { Object.defineProperty(e, r, Object.getOwnPropertyDescriptor(t, r)); }); } return e; }
+function _defineProperty(obj, key, value) { key = _toPropertyKey(key); if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+function _toPropertyKey(arg) { var key = _toPrimitive(arg, "string"); return _typeof(key) === "symbol" ? key : String(key); }
+function _toPrimitive(input, hint) { if (_typeof(input) !== "object" || input === null) return input; var prim = input[Symbol.toPrimitive]; if (prim !== undefined) { var res = prim.call(input, hint || "default"); if (_typeof(res) !== "object") return res; throw new TypeError("@@toPrimitive must return a primitive value."); } return (hint === "string" ? String : Number)(input); }
+(function (window) {
+  if (!window.app) {
+    window.app = {};
+  }
+  var breakpoints = {
+    xxl: 1919,
+    xl: 1439,
+    lg: 1279,
+    md: 991,
+    sm: 575
+  };
+  var events = {}; // кастомные события
+
+  window.app.config = {
+    events: events,
+    breakpoints: breakpoints
+    // ...
+  };
+})(window);
+(function (window) {
+  if (!window.app) {
+    window.app = {};
+  }
+  var checkResponse = function checkResponse(res) {
+    if (res.ok) {
+      return res.json();
+    }
+    return Promise.reject("\u041E\u0448\u0438\u0431\u043A\u0430 ".concat(res.status));
+  };
+  var checkResponseSuccess = function checkResponseSuccess(res) {
+    if (res && res.success) {
+      return res;
+    }
+    return Promise.reject("\u041E\u0442\u0432\u0435\u0442 \u043D\u0435 success: ".concat(res));
+  };
+  var buildHttpClient = function buildHttpClient(baseUrl) {
+    return function (endpoint) {
+      var options = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : {};
+      return fetch("".concat(baseUrl).concat(endpoint), options).then(checkResponse).then(checkResponseSuccess);
+    };
+  };
+  var setObserver = function setObserver(element, handleObserve) {
+    var manualConfig = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : {};
+    var config = _objectSpread({
+      childList: true
+    }, manualConfig);
+    var observer = new MutationObserver(function () {
+      return handleObserve(element);
+    });
+    observer.observe(element, config);
+  };
+  var findAncestorsByClassName = function findAncestorsByClassName(el, className) {
+    var stopElement = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : null;
+    var ancestorEls = [];
+    var currentParent = el.parentElement;
+    if (!currentParent) {
+      return ancestorEls;
+    }
+    while (currentParent !== null && currentParent !== stopElement) {
+      if (currentParent.classList.contains(className)) {
+        ancestorEls = [].concat(_toConsumableArray(ancestorEls), [currentParent]);
+      }
+      currentParent = currentParent.parentElement;
+    }
+    return ancestorEls;
+  };
+  var findAncestorByClassName = function findAncestorByClassName(el, className) {
+    var ancestorEl = el.parentElement;
+    while (!ancestorEl.classList.contains(className)) {
+      ancestorEl = ancestorEl.parentElement;
+      if (!ancestorEl) {
+        return null;
+      }
+    }
+    return ancestorEl;
+  };
+  var buildComponentLogger = function buildComponentLogger(componentName) {
+    return function (text) {
+      var context = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : '';
+      var data = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : null;
+      var msg = context ? "".concat(componentName, ":").concat(context, ":").concat(text) : "".concat(componentName, ":").concat(text);
+      console.debug(msg);
+      if (data) {
+        console.dir(data);
+      }
+    };
+  };
+  var debounce = function debounce(callee, timeoutMs) {
+    return function perform() {
+      for (var _len = arguments.length, args = new Array(_len), _key = 0; _key < _len; _key++) {
+        args[_key] = arguments[_key];
+      }
+      var previousCall = this.lastCall;
+      this.lastCall = Date.now();
+      if (previousCall && this.lastCall - previousCall <= timeoutMs) {
+        clearTimeout(this.lastCallTimer);
+      }
+      this.lastCallTimer = setTimeout(function () {
+        return callee.apply(void 0, args);
+      }, timeoutMs);
+    };
+  };
+  var throttle = function throttle(callee, timeout) {
+    var timer = null;
+    return function perform() {
+      for (var _len2 = arguments.length, args = new Array(_len2), _key2 = 0; _key2 < _len2; _key2++) {
+        args[_key2] = arguments[_key2];
+      }
+      if (timer) return;
+      timer = setTimeout(function () {
+        callee.apply(void 0, args);
+        clearTimeout(timer);
+        timer = null;
+      }, timeout);
+    };
+  };
+  window.app.lib = {
+    setObserver: setObserver,
+    findAncestorsByClassName: findAncestorsByClassName,
+    findAncestorByClassName: findAncestorByClassName,
+    buildComponentLogger: buildComponentLogger,
+    debounce: debounce,
+    throttle: throttle,
+    checkResponse: checkResponse,
+    checkResponseSuccess: checkResponseSuccess,
+    buildHttpClient: buildHttpClient
+  };
+})(window);
+window.onload = function () {
+  slideMin();
+  slideMax();
+};
+var minVal = document.querySelector(".range__min");
+var maxVal = document.querySelector(".range__max");
+var priceInputMin = document.querySelector(".input-wrap__min");
+var priceInputMax = document.querySelector(".input-wrap__max");
+var maxTooltip = document.querySelector(".tooltip_max");
+var minGap = 0;
+var range = document.querySelector(".range__track");
+var sliderMinValue = parseInt(minVal.min);
+var sliderMaxValue = parseInt(maxVal.max);
+function slideMin() {
+  var gap = parseInt(maxVal.value) - parseInt(minVal.value);
+  if (gap <= minGap) {
+    minVal.value = parseInt(maxVal.value) - minGap;
+  }
+  priceInputMin.value = minVal.value;
+  setArea();
+}
+function slideMax() {
+  var gap = parseInt(maxVal.value) - parseInt(minVal.value);
+  if (gap <= minGap) {
+    maxVal.value = parseInt(minVal.value) + minGap;
+  }
+  maxTooltip.innerHTML = maxVal.value + '$';
+  priceInputMax.value = maxVal.value;
+  setArea();
+}
+function setArea() {
+  range.style.left = "".concat((minVal.value - sliderMinValue) / (sliderMaxValue - sliderMinValue) * 100, "%");
+  range.style.right = "".concat(100 - (maxVal.value - sliderMinValue) / (sliderMaxValue - sliderMinValue) * 100, "%");
+  maxTooltip.style.right = 100 - maxVal.value / sliderMaxValue * 100 + "%";
+}
+function setMinInput() {
+  var minPrice = parseInt(priceInputMin.value);
+  if (minPrice < sliderMinValue) {
+    priceInputMin.value = sliderMinValue;
+  }
+  minVal.value = priceInputMin.value;
+  slideMin();
+}
+function setMaxInput() {
+  var maxPrice = parseInt(priceInputMax.value);
+  if (maxPrice > sliderMaxValue) {
+    priceInputMax.value = sliderMaxValue;
+  }
+  maxVal.value = priceInputMax.value;
+  slideMax();
+}
+var dropDownTitleAll = document.querySelectorAll('.dropdown__title');
+dropDownTitleAll.forEach(function (item) {
+  item.addEventListener('click', function () {
+    item.classList.toggle('click');
+  });
+});
+var dropDownCheckboxes = document.querySelectorAll('.dropdown__checkbox');
+var badgesList = document.querySelector('.badges');
+dropDownCheckboxes.forEach(function (checkbox) {
+  checkbox.addEventListener('change', function () {
+    var checkboxValue = this.value;
+    if (this.checked) {
+      var badge = document.createElement('li');
+      badge.classList.add('badges__item');
+      badgesList.appendChild(badge);
+      var badgeName = document.createElement('span');
+      badgeName.classList.add('badges__name');
+      badge.appendChild(badgeName);
+      badgeName.textContent = checkboxValue;
+      var badgeClose = document.createElement('button');
+      badgeClose.classList.add('badges__btn');
+      badge.appendChild(badgeClose);
+      badgeClose.addEventListener('click', function () {
+        badgesList.removeChild(badge);
+        dropDownCheckboxes.forEach(function (cb) {
+          if (cb.value === checkboxValue) {
+            cb.checked = false;
+          }
+        });
+      });
+    } else {
+      var badgesItem = document.querySelectorAll('.badges__item');
+      badgesItem.forEach(function (badge) {
+        if (badge.textContent === checkboxValue) {
+          badgesList.removeChild(badge);
+        }
+      });
+    }
+  });
+});
